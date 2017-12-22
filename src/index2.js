fc28c537f33e2b79e0f9e9c3531a9562467162c4
kevingrond <kevingrond@gmail.com>
2017-12-22 20:08:00 +0100

    Table drawing function that draws out the data from JSON

--- src/index2.js
+++ src/index2.js
@@ -51,3 +51,48 @@ function removearray() {
     console.log(data);
 }
 
+function killrow(i) {
+    jsonfile.intro.splice(i, 1);
+    console.log(jsonfile)
+}
+
+function CreateTableFromJSON() {
+
+    var col = [];
+    for (var i = 0; i < jsonfile.intro.length; i++) {
+        for (var key in jsonfile.intro[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+    console.log(col);
+
+    var table = document.createElement("table");
+
+    var tr = table.insertRow(-1);
+
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+
+
+    for (var i = 0; i < jsonfile.intro.length; i++) {
+
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < (col.length); j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = jsonfile.intro[i][col[j]];
+        }
+
+
+    }
+
+    var divContainer = document.getElementById("showData");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+}
